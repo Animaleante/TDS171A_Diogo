c0c16a695adb8d2ba3809010d54fbc32839354b5
Lab. Desenvolvimento <Lab. Desenvolvimento@RB-LAB102-15>
2017-08-22 21:35:39 -0300

    Sei la

---
+++ TDS171A_Diogo/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TDS171A_Diogo.Models;
+
+namespace TDS171A_Diogo.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Aps_Net_MVC_CS") { }
+
+        public DbSet<Category> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ TDS171A_Diogo/Models/Fabricante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDS171A_Diogo.Models
+{
+    public class Fabricante
+    {
+
+        public long FabricanteId { get; set; }
+        public string nome { get; set; }
+    }
+}
