2a539dc6efe593742b1e31030206b8cbe0e73e4c
Diogo Sota Moraes <diogo@cht.net.br>
2017-08-30 11:06:10 -0300

    Add project files.

---
+++ TestWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebApplication.Contexts;
+
+namespace TestWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Categorias = context.Categorias.OrderBy(c => c.Nome).ToArray();
+            ViewBag.Fabricantes = context.Fabricantes.OrderBy(c => c.Nome).ToArray();
+            ViewBag.Produtos = context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).OrderBy(n => n.Nome);
+
+            return View(context.Categorias);
+        }
+    }
+}
