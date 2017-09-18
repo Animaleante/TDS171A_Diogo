1b7562f377ff00db2a68b572607d50905a64ea1d
Diogo S. Moraes <srto.dih@gmail.com>
2017-09-17 23:05:32 -0300

    Fix do novo nome de namespace

--- TestWebApplication/Properties/AssemblyInfo.cs
+++ TestWebApplication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestWebApplication")]
+[assembly: AssemblyTitle("TDS171A_Prog_Visual")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestWebApplication")]
+[assembly: AssemblyProduct("TDS171A_Prog_Visual")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
